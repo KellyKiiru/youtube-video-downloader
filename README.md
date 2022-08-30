67523b3c1a3a71387be23e6cf8d048186ccb3f72
KellyKiiru <infowithkiiru@gmail.com>
2022-08-30 23:43:32 +0300

    add readme markdown and minor change to url in main file

---
+++ README.md
@@ -0,0 +1,3 @@
+Youtube Video Downloader is a simple python program for downloading youtube videos at the highest quality.
+
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tokenize import maybe
 from pytube import YouTube
 
-url = 'https://www.youtube.com/watch?v=PJxxfilLnGI'
+"""
+Replace url with your preferred video url then run the script.
+The video will be downloaded in the same folder that contains this file
+"""
+
+url = 'https://www.youtube.com/watch?v=UprcpdwuwCg&list=RDUKunvvN2iCk&index=15'
 
 my_video = YouTube(url)
 
@@ -9,6 +14,5 @@ my_video = YouTube(url)
 print(my_video.title)
 
 my_video = my_video.streams.get_highest_resolution()
-
-
+    
 my_video.download()
